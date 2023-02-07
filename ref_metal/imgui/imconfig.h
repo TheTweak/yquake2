b7d7082f7bcc1c757a627a8e1faa308dc52b532a
thetweak <thetweak@cloud.upwork.com>
2023-02-07 17:56:49 +0300

    add imgui

---
+++ ref_metal/imgui/imconfig.h
@@ -0,0 +1,13 @@
+//
+//  imconfig.h
+//  yq2
+//
+//  Created by SOROKIN EVGENY on 07.02.2023.
+//
+
+#ifndef imconfig_h
+#define imconfig_h
+
+#define IMGUI_IMPL_METAL_CPP
+
+#endif /* imconfig_h */
